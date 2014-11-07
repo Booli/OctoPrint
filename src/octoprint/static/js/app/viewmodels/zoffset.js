8f3b22c512c9f949086dfbf8bb1be56ca7443cc9
Pim Rutgers <pim.rutgers@gmail.com>
2014-11-07 12:12:40 +0100

    calibration_zOffset

--- src/octoprint/static/js/app/viewmodels/zoffset.js
+++ src/octoprint/static/js/app/viewmodels/zoffset.js
@@ -4,18 +4,9 @@ function ZOffsetViewModel(settingsViewModel, controlViewModel) {
     self.settings = settingsViewModel;
     self.control = controlViewModel;
 
-    self.calibrationZOffset = ko.observable(undefined);
+    self.calibration_zOffset = ko.observable(undefined);
 
-    self.calibrationZOffset = self.settings.printer_zOffset();
-
-    self.startZOffset = function() {
-        $("#zOffset_dialog").modal("show");
-            var old_zOffset = self.settings.printer_zOffset();
-            calibrationZOffset = old_z0ffset - 0.5;
-
-            self.control.sendHomeCommand(z);
-            self.control.sendJogCommand(z, 1, calibrationZ);
-
+    self.onStartup = function() {
+        self.calibration_zOffset(self.settings.printer_zOffset());
     };
-
 }
