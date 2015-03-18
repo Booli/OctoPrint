eb7f70f6dd6143aa6e55e31562f70233bd0aac55
Pim Rutgers <pim.rutgers@gmail.com>
2015-03-18 11:53:29 +0200

    Adding empty gcode scripts

--- src/octoprint/settings.py
+++ src/octoprint/settings.py
@@ -205,6 +205,8 @@ default_settings = {
 	"scripts": {
 		"gcode": {
 			"afterPrintCancelled": "; disable motors\nM84\n\n;disable all heaters\n{% snippet 'disable_hotends' %}\nM140 S0\n\n;disable fan\nM106 S0",
+			"beforePrintStarted": "",
+			"afterPrinterConnected": "",
 			"snippets": {
 				"disable_hotends": "{% for tool in range(printer_profile.extruder.count) %}M104 T{{ tool }} S0\n{% endfor %}"
 			}
